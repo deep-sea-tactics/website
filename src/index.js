d47c06e078d174b0de74fcc060c84c888b31c9f1
William Faircloth <willfaircloth@icloud.com>
2022-12-19 16:19:40 +0000

    parcel

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-let currentImage = 1;
-
-setInterval(function() {
-    $("#carousel").css("background-image", "url('images/" + currentImage + ".jpg')");
-    currentImage++;
-    if (currentImage === 17) currentImage = 1;
-}, 4000);
+function startCarousel() {
+    let currentImage = 1;
+    setInterval(function() {
+        $("#carousel").css("background-image", "url('images/" + currentImage + ".jpg')");
+        currentImage++;
+        if (currentImage === 17) currentImage = 1;
+    }, 4000);
+};
