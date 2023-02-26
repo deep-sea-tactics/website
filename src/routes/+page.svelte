cabdba6e3f75577fb0ea245cade9c0a94bab24dc
William Faircloth <willfaircloth@icloud.com>
2023-02-26 20:05:39 +0000

    chore: remove unused import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
     import Navbar from "../lib/Navbar.svelte";
     import Jumbotron from "../lib/Jumbotron.svelte";
-    import Section from "../lib/Section/Section.svelte";
 </script>
 <Navbar></Navbar>
 <Jumbotron></Jumbotron>
