d1497a0e787ff08eff44595d91f0b3825b42964b
William Faircloth <willfaircloth@icloud.com>
2023-02-26 20:22:30 +0000

    feat: add about section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
     import Navbar from "../lib/Navbar.svelte";
     import Jumbotron from "../lib/Jumbotron.svelte";
+    import About from "../lib/Section/About.svelte";
 </script>
 <Navbar></Navbar>
 <Jumbotron></Jumbotron>
+<About></About>
