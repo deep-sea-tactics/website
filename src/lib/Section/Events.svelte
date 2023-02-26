d847c7fc5dc512fc01718107baacccb79c7c5612
William Faircloth <willfaircloth@icloud.com>
2023-02-26 21:14:07 +0000

    feat: create events section

---
+++ src/lib/Section/Events.svelte
@@ -0,0 +1,39 @@
+<script>
+    import Section from "./Section.svelte";
+    import Item from "../Item.svelte";
+</script>
+<style>
+    .date {
+        text-transform: uppercase;
+        font-family: Roboto;
+        font-weight: 700;
+        text-align: center;
+    }
+
+    .date h3 {
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+
+    .month {
+        font-size: 20px;
+    }
+
+    .day {
+        font-size: 36px;
+    }
+</style>
+<Section header="Events">
+    <Item value="Compete in the regional competition at Old Dominion University.">
+        <div class="date">
+            <h3 class="month">May</h3>
+            <h3 class="day">13th</h3>
+        </div>
+    </Item>
+    <Item value="Compete in the international competition in Longmont, Colorado.">
+        <div class="date">
+            <h3 class="month">June</h3>
+            <h3 class="day">22nd</h3>
+        </div>
+    </Item>
+</Section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,9 @@
     import Navbar from "../lib/Navbar.svelte";
     import Jumbotron from "../lib/Jumbotron.svelte";
     import About from "../lib/Section/About.svelte";
+    import Events from "../lib/Section/Events.svelte";
 </script>
 <Navbar></Navbar>
 <Jumbotron></Jumbotron>
 <About></About>
+<Events></Events>
