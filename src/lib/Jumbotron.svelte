4348256d6babdac3165d20d80e000c302a679021
William Faircloth <willfaircloth@icloud.com>
2023-02-26 18:50:39 +0000

    feat: jumbotron

--- src/lib/Jumbotron.svelte
+++ src/lib/Jumbotron.svelte
@@ -1,3 +1,28 @@
-<style></style>
+<style>
+    .jumbotron {
+        background-image: url('/images/jumbotron.png');
+        background-size: cover;
+        background-position: center;
+        height: 513px;
+        width: 100%;
+        display: flex;
+    }
+
+    .jumbotron h1 {
+        font-family: 'Roboto';
+        color: white;
+        font-weight: 600;
+        line-height: 60px;
+        font-size: 48px;
+        max-width: 855px;
+        font-style: normal;
+
+        /* position as in prototype, which is not yet finalized. */
+        position: relative;
+        top: 70px;
+        left: 39px;
+    }
+</style>
 <div class="jumbotron">
+    <h1>Landstown High School's innovative underwater robotics team.</h1>
 </div>
