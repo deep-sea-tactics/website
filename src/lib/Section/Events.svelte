647f524a5f5df1703b116e6cf695786d5f27996a
William Faircloth <willfaircloth@icloud.com>
2023-02-26 22:31:08 +0000

    feat: add top and bottom margin like prototype

--- src/lib/Section/Events.svelte
+++ src/lib/Section/Events.svelte
@@ -22,18 +22,25 @@
     .day {
         font-size: 36px;
     }
+
+    .item-wrapper {
+        margin-top: 2.5em;
+        margin-bottom: 2.5em;
+    }
 </style>
 <Section header="Events">
-    <Item value="Compete in the regional competition at Old Dominion University.">
-        <div class="date">
-            <h3 class="month">May</h3>
-            <h3 class="day">13th</h3>
-        </div>
-    </Item>
-    <Item value="Compete in the international competition in Longmont, Colorado.">
-        <div class="date">
-            <h3 class="month">June</h3>
-            <h3 class="day">22nd</h3>
-        </div>
-    </Item>
+    <div class="item-wrapper">
+        <Item value="Compete in the regional competition at Old Dominion University.">
+            <div class="date">
+                <h3 class="month">May</h3>
+                <h3 class="day">13th</h3>
+            </div>
+        </Item>
+        <Item value="Compete in the international competition in Longmont, Colorado.">
+            <div class="date">
+                <h3 class="month">June</h3>
+                <h3 class="day">22nd</h3>
+            </div>
+        </Item>
+    </div>
 </Section>
