27d07a7949d0b1e305860caa67c20c4890bd7f18
William Faircloth <willfaircloth@icloud.com>
2023-02-26 20:05:18 +0000

    feat: create section component

---
+++ src/lib/Section/Section.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    export let header: string;
+
+    type BrandColor = "#f2f2f2" | "#2C3289";
+    export let backgroundColor: BrandColor = "#f2f2f2";
+    const color = backgroundColor === "#f2f2f2" ? "000000" : "#f2f2f2";
+</script>
+<style>
+    .section {
+        padding-top: 2em;
+        padding-left: 2em;
+    }
+
+    .section h3 {
+        font-family: 'Bebas Neue';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 48px;
+        line-height: 58px;
+        margin-bottom: 0px;
+    }
+
+    .section hr {
+        width: 570px;
+        margin-left: 0px;
+        margin-top: 20px;
+        margin-bottom: 20px;
+        height: 0.35px;
+    }
+</style>
+<div class="section" style="background-color: {backgroundColor}; color: {color}">
+    <h3>{header}</h3>
+    <hr style="background-color: {color}">
+    <slot>
+    </slot>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import Navbar from "../lib/Navbar.svelte";
     import Jumbotron from "../lib/Jumbotron.svelte";
+    import Section from "../lib/Section/Section.svelte";
 </script>
 <Navbar></Navbar>
 <Jumbotron></Jumbotron>
