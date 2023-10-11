e5710e7c19af9aa171d530006e0e187abdcb1d9d
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2023-10-11 18:00:29 +0000

    Add event description and image

--- src/lib/section/Events.svelte
+++ src/lib/section/Events.svelte
@@ -1,33 +1,36 @@
 <script lang="ts">
 	import Section from './Section.svelte';
-	import Item from '../Item.svelte';
-
-	let months = ["January", "Februrary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-	let days = ["0th", "1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th", "13th", "14th", "15th", "16th", "17th", "18th", "19th", "20th", "21st", "22nd", "23rd", "24th", "25th", "26th", "27th" ,"28th", "29th", "30th", "31st"]
 
 	interface Event {
 		title: string,
 		description: string;
-		date: number;
+		date: Date | null;
 	}
 
 	const events: Event[] = [
 		{
-			title: 'Regional Competiton',
+			title: '2023 Regional Competiton',
 			description: 'Compete in the regional competition at Old Dominion University.',
-			date: new Date().setFullYear(2023, 5, 13)
+			date: new Date(new Date().setFullYear(2023, 4, 13))
 		},
 		{
-			title: "International Competiton",
+			title: "2023 International Competiton",
 			description: 'Compete in the international competition in Longmont, Colorado.',
-			date: new Date().setFullYear(2023, 6, 22)
+			date: new Date(new Date().setFullYear(2023, 5, 22))
+		},
+		{
+			title: '2024 Regional Competiton',
+			description: 'Compete in the regional competition at Old Dominion University.',
+			date: null
+		},
+		{
+			title: "2024 International Competiton",
+			description: 'Compete in the international competition in Longmont, Colorado.',
+			date: null
 		}
 	];
-	let getDateString = (date: number) => {
-		let newDate = new Date(date);
-		let month = months[newDate.getMonth()]
-		let day = days[newDate.getDate()]
-		return `${month} ${day}, ${newDate.getFullYear()}`
+	let getDateString = (date: Date) => {
+		return new Intl.DateTimeFormat("en-US", { month: "long", day: "2-digit", year: "numeric" }).format(date);
 	}
 </script>
 
@@ -35,19 +38,69 @@
 	<Section header="Events">
 		<div class="item-wrapper">
 			{#each events as event}
+			<div class="eventWrap">
 				<div class="event">
-					<h2>{event.title}</h2>
-					<h3>{getDateString(event.date)}</h3>
+					<div class="eventInner">
+						<div class="eventTitle">
+							<h2>{event.title}</h2>
+							{#if event.date != null}
+								<h3>{getDateString(event.date)}</h3>
+							{:else}
+								<h3>Unscheduled</h3>
+							{/if}
+						</div>
+						
+						<div class="description">
+							{event.description}
+						</div>
+					</div>
+					
 				</div>
+			</div>
 			{/each}
 		</div>
 	</Section>
 </div>
 
 <style>
-
 	.item-wrapper {
 		margin-top: 2rem;
 		margin-bottom: 2rem;
+		display: flex;
+		flex-direction: row;
+		border-radius: 1rem;
+	}
+
+	.eventWrap {
+		min-width: 350px;
+		padding-right: 20px;
+		box-sizing: border-box;
+	}
+	.event {
+		
+		color: var(--textColor2);
+		z-index: 1;
+		backdrop-filter: blur(1px);
+		background-position: center;
+		background-size: cover;
+		background-image: url("/images/jumbotron.png");
+	}
+	.eventInner {
+		background: rgba(0, 0, 0, 0.3);
+		width: 100%;
+		height: 100%;
+		box-sizing: border-box;
+	}
+
+	.eventTitle {
+		padding: 5px 20px;
+	}
+
+	.description {
+		width: 100%;
+		padding: 10px 20px;
+		margin-top: 5vh;
+		background: rgba(0, 0, 0, 0.3);
+		box-sizing: border-box;
 	}
 </style>
