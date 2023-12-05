cfaaea82ca9e4b0ceb3104cda814f75f8b35c5c7
OgaFr <153020530+OgaFr@users.noreply.github.com>
2023-12-05 15:05:56 -0500

    Update +page.svelte (#64)

--- src/routes/sponsor/+page.svelte
+++ src/routes/sponsor/+page.svelte
@@ -6,6 +6,11 @@
 
 <main>
 	<h1>Sponsors</h1>
+
+<Section header="Marketing">
+	<p>Marketing is the backbone of the team; this branch allows the rest of the team to function. Marketing members are responsible for fundraising thousands of dollars, paying for expenses such as travel costs and purchase orders for parts. The marketing team is also responsible for creating advertisements that increase engagement with the school population. Lastly, the marketing team is responsible for filling out the technical documentation, and documenting everything that the team has done, including the design of the robot.</p>
+</Section>
+
 </main>
 
 <style>
