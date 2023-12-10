74e1e111275c7939497b451d55cda0c540e00bc0
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-12-10 18:21:15 -0500

    feat: page hyperlinks

---
+++ src/routes/tasks/float/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	onMount(() =>
+		goto(
+			'https://20693798.fs1.hubspotusercontent-na1.net/hubfs/20693798/Preview%20Mission%20RNPN%20Final.pdf'
+		)
+	);
+</script>
---
+++ src/routes/tasks/overview/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	onMount(() =>
+		goto(
+			'https://20693798.fs1.hubspotusercontent-na1.net/hubfs/20693798/2024_MATE_ROV_Competition_Briefing_Final%5B61%5D.pdf'
+		)
+	);
+</script>
