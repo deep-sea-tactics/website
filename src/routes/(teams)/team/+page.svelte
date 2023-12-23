3452903a35ac9112db61db9d94f56012356c61c5
Coaster <56899612+CoasterFan5@users.noreply.github.com>
2023-12-22 21:04:01 -0500

    fix circle stuff

--- src/routes/(teams)/team/+page.svelte
+++ src/routes/(teams)/team/+page.svelte
@@ -36,16 +36,19 @@
 <div class="wrapper">
 	<main>
 		{#each teams as team}
-			<div class="team" style="--bg: {team.color};">
+			<a class="team" style="--bg: {team.color};" href="/{team.name.toLowerCase()}">
 				<div class="circle">
 					<Circle>
 						{@html team.name + "&nbsp;".repeat(20) + team.name + "&nbsp;".repeat(20)}
 					</Circle>
 				</div>
-				<a class="icon" href="/{team.name.toLowerCase()}">
+				<div class="icon" >
+					<div class="iconBG">
+
+					</div>
 					<img src="/icons/teams/{team.name.toLowerCase()}.svg" alt="{team.name} Logo" />
-				</a>
-			</div>
+				</div>
+			</a>
 		{/each}
 	</main>
 </div>
@@ -65,11 +68,11 @@
 		flex-wrap: wrap;
 		justify-content: center;
 		max-width: calc((310px * 3) + 6rem + (5px * 6));
-		font-family: 'Bebas Neue', sans-serif;
 		letter-spacing: 0.2rem;
 	}
 
 	.team {
+		position: relative;
 		margin: 1rem;
 		border: 5px solid var(--bg);
 		border-radius: 50%;
@@ -79,27 +82,56 @@
 	}
 
 	.icon {
-		position: relative;
-		top: -50%;
-		left: 50%;
-		display: inline-block;
-		transform: translate(-50%, -30%);
-		padding: 2rem;
-		border-radius: 50%;
-		background-color: var(--bg);
-		transition: all 0.2s ease-in-out;
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		left: 0px;
+		top: 0px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		
 	}
 
-	.icon:hover {
-		transform: translate(-50%, -30%) scale(1.1);
+	.icon img {
+		width: 25%;
+		
+		position: relative;
+	}
+	.iconBG {
+		width: 25%;
+		position: absolute;
+		background: var(--bg);
+		height: 25%;
+		padding: 2rem;
+		border-radius: 50%;
+
 	}
 
 	.circle {
-		position: relative;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-		width: calc(100% - 2rem);
-		height: calc(100% - 2rem);
+		position: absolute;
+		top: 0px;
+		left: 0px;
+		width: 100%;
+		height: 100%;
+	}
+	.team:hover {
+		scale: 1.1;
+		transition: all cubic-bezier(0.075, 0.82, 0.165, 1) 0.5s;
+	}
+	.team:hover .circle {
+		animation: rotate infinite linear 5s;
+		animation-iteration-count: infinite;
+		animation-timing-function: linear;
+		
+	}
+
+	@keyframes rotate {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
 	}
 </style>
