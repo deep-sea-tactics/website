c0ab9739e47fe844507c450547cb3fff0b2c95c0
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-12-22 21:11:33 -0500

    fix: don't reset rotation after leaving hover

--- src/routes/(teams)/team/+page.svelte
+++ src/routes/(teams)/team/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { title } from '$lib/Jumbotron.svelte';
 	import { Circle } from 'svelte-text-path';
+	import Team from './Team.svelte';
 
 	$title = 'Deep Sea Tactics Teams';
 
@@ -36,19 +37,7 @@
 <div class="wrapper">
 	<main>
 		{#each teams as team}
-			<a class="team" style="--bg: {team.color};" href="/{team.name.toLowerCase()}">
-				<div class="circle">
-					<Circle>
-						{@html team.name + "&nbsp;".repeat(20) + team.name + "&nbsp;".repeat(20)}
-					</Circle>
-				</div>
-				<div class="icon" >
-					<div class="iconBG">
-
-					</div>
-					<img src="/icons/teams/{team.name.toLowerCase()}.svg" alt="{team.name} Logo" />
-				</div>
-			</a>
+			<Team name={team.name} color={team.color} />
 		{/each}
 	</main>
 </div>
@@ -69,69 +58,5 @@
 		justify-content: center;
 		max-width: calc((310px * 3) + 6rem + (5px * 6));
 		letter-spacing: 0.2rem;
-	}
-
-	.team {
-		position: relative;
-		margin: 1rem;
-		border: 5px solid var(--bg);
-		border-radius: 50%;
-		width: 310px;
-		height: 310px;
-		aspect-ratio: 1 1/ 1;
-	}
-
-	.icon {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		left: 0px;
-		top: 0px;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		
-	}
-
-	.icon img {
-		width: 25%;
-		
-		position: relative;
-	}
-	.iconBG {
-		width: 25%;
-		position: absolute;
-		background: var(--bg);
-		height: 25%;
-		padding: 2rem;
-		border-radius: 50%;
-
-	}
-
-	.circle {
-		position: absolute;
-		top: 0px;
-		left: 0px;
-		width: 100%;
-		height: 100%;
-	}
-	.team:hover {
-		scale: 1.1;
-		transition: all cubic-bezier(0.075, 0.82, 0.165, 1) 0.5s;
-	}
-	.team:hover .circle {
-		animation: rotate infinite linear 5s;
-		animation-iteration-count: infinite;
-		animation-timing-function: linear;
-		
-	}
-
-	@keyframes rotate {
-		0% {
-			transform: rotate(0deg);
-		}
-		100% {
-			transform: rotate(360deg);
-		}
 	}
 </style>
