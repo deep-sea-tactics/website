d9fefb2930f41fa9be81e54e6865c95962f0754f
OgaFr <153020530+OgaFr@users.noreply.github.com>
2023-12-24 16:32:07 -0500

    Design desc

--- src/routes/(teams)/design/+page.svelte
+++ src/routes/(teams)/design/+page.svelte
@@ -4,7 +4,6 @@
 
 <Section header="Design">
 	<p>
-		The design team is an integral part of the club. From creating prototypes of the ROV to
-		researching materials, the design team helps bring the ROV to life.
+		The design of the robot must first be created for any branch to properly function. From creating prototypes of the ROV to researching materials, the design team helps bring the ROV to life. The design team mainly uses Autodesk’s Fusion 360 app or OnShape as an alternative. The design team works on modeling the robot in Fusion 360 and printing out different objects to support the robot. Once final designs have been submitted, the design team looks over all the designs to offer constructive criticism, and a final vote is made.
 	</p>
 </Section>
