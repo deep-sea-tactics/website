e74f8f95a8757578d6fc7bb9f84d83a8a7aacb4a
William Faircloth <willuhmjs@gmail.com>
2023-12-23 22:02:11 -0500

    feat: try and position

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,18 @@
-<header>
-    <nav class="font-medium text-lg flex justify-between mx-4 my-3 items-center">
-        <img src="https://via.placeholder.com/200x75" alt="placeholder">
-        <ul class="flex flex-row gap-2">
+<header class="flex justify-between flex-col h-screen items-center">
+    <div class="">
+
+    <nav class="font-medium text-lg flex justify-between mx-4 my-1 items-center">
+        <img src="horizontal_logo.png" alt="placeholder" class="h-20">
+        <ul class="flex flex-row gap-4">
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
         </ul>
     </nav>
-    <div class="hero">
-        <div>
+        <div class="">
             <h2 class="text-5xl font-bold">Landstown High School's innovative underwater robotics team</h2>
         </div>
+    </div>
+
         <div class="flex md:flex-row flex-col justify-evenly overflow-x-hidden gap-2">
             <img src="https://via.placeholder.com/320x180" alt="placeholder">
             <img src="https://via.placeholder.com/320x180" alt="placeholder">
@@ -17,7 +20,6 @@
             <img src="https://via.placeholder.com/320x180" alt="placeholder">
             <img src="https://via.placeholder.com/320x180" alt="placeholder">
         </div>
-    </div>
 </header>
 
 <main>
@@ -25,3 +27,9 @@
 </main>
 <footer>
 </footer>
+
+<style>
+    * {
+        border: 1px solid black;
+    }
+</style>
