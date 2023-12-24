96b11610e35aaab2591da014907aa2687de764c8
William Faircloth <willuhmjs@gmail.com>
2023-12-23 21:29:27 -0500

    feat: nav, image gaps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <header>
-    <nav class="font-medium">
+    <nav class="font-medium text-lg flex justify-between mx-4 my-3 items-center">
+        <img src="https://via.placeholder.com/200x75" alt="placeholder">
         <ul class="flex flex-row gap-2">
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
@@ -7,9 +8,9 @@
     </nav>
     <div class="hero">
         <div>
-            <h2 class="text-6xl font-bold">Landstown High School's innovative underwater robotics team</h2>
+            <h2 class="text-5xl font-bold">Landstown High School's innovative underwater robotics team</h2>
         </div>
-        <div class="flex md:flex-row flex-col justify-evenly overflow-x-hidden">
+        <div class="flex md:flex-row flex-col justify-evenly overflow-x-hidden gap-2">
             <img src="https://via.placeholder.com/320x180" alt="placeholder">
             <img src="https://via.placeholder.com/320x180" alt="placeholder">
             <img src="https://via.placeholder.com/320x180" alt="placeholder">
