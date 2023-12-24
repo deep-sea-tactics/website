ddb3d97c8e5e8783ec48b62468bcaf2164bc6f0d
William Faircloth <willuhmjs@gmail.com>
2023-12-24 14:07:25 -0500

    feat: smaller text, center headline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,34 @@
-<header class="flex justify-between flex-col h-screen items-center">
-    <div class="">
-
-    <nav class="font-medium text-lg flex justify-between mx-4 my-1 items-center">
+<header class="flex flex-col h-screen justify-between">
+    <nav class="font-medium text-md flex justify-between items-center px-4 py-1 w-full">
         <img src="horizontal_logo.png" alt="placeholder" class="h-20">
         <ul class="flex flex-row gap-4">
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
         </ul>
     </nav>
-        <div class="">
-            <h2 class="text-5xl font-bold">Landstown High School's innovative underwater robotics team</h2>
-        </div>
-    </div>
 
-        <div class="flex md:flex-row flex-col justify-evenly overflow-x-hidden gap-2">
-            <img src="https://via.placeholder.com/320x180" alt="placeholder">
-            <img src="https://via.placeholder.com/320x180" alt="placeholder">
-            <img src="https://via.placeholder.com/320x180" alt="placeholder">
-            <img src="https://via.placeholder.com/320x180" alt="placeholder">
-            <img src="https://via.placeholder.com/320x180" alt="placeholder">
-        </div>
+    <div class="flex items-center justify-center text-center mx-auto max-w-screen-lg">
+        <h2 class="text-4xl font-bold">Landstown High School's innovative underwater robotics team</h2>
+    </div>
+    
+
+    <div class="flex md:flex-row flex-col justify-evenly overflow-x-hidden gap-2">
+        <!-- TODO: Our images go here -->
+        <img src="https://via.placeholder.com/320x180" alt="placeholder">
+        <img src="https://via.placeholder.com/320x180" alt="placeholder">
+        <img src="https://via.placeholder.com/320x180" alt="placeholder">
+        <img src="https://via.placeholder.com/320x180" alt="placeholder">
+        <img src="https://via.placeholder.com/320x180" alt="placeholder">
+        <img src="https://via.placeholder.com/320x180" alt="placeholder">
+        <img src="https://via.placeholder.com/320x180" alt="placeholder">
+
+    </div>
 </header>
 
 <main>
-
+    <!-- TODO: Our main content goes here -->
 </main>
-<footer>
-</footer>
 
-<style>
-    * {
-        border: 1px solid black;
-    }
-</style>
+<footer>
+    <!-- TODO: Our footer content goes here -->
+</footer>
