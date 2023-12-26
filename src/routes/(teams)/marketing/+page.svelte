a09a55adfc05c2f4902a6fb0bfc69beba5e50812
OgaFr <153020530+OgaFr@users.noreply.github.com>
2023-12-25 19:39:40 -0500

    Marketing team synopsis modifications

--- src/routes/(teams)/marketing/+page.svelte
+++ src/routes/(teams)/marketing/+page.svelte
@@ -5,10 +5,10 @@
 <Section header="Marketing">
 	<p>
 		Marketing is the backbone of the team; this branch allows the rest of the team to function.
-		Marketing members are responsible for fundraising thousands of dollars,
-		paying for expenses such as travel costs and purchase orders for parts.
-		The marketing team is also responsible for creating advertisements that increase engagement with the school population.
-		Lastly, the marketing team is responsible for filling out the technical documentation, and documenting everything that the team has done,
-		including the design of the robot.
+		Marketing members are responsible for raising the thousands of dollars
+		needed for travel costs and purchase orders for parts.
+		The marketing team is also responsible for creating advertisements that increase engagement with the school population as well as
+		filling out the technical documentation, the documentation of everything that the team has done,
+		including the design of the robot, the safety precautions taken during the building, and the expenses of things we bought or paid for.
 	</p>
 </Section>
