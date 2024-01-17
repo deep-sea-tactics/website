0800d8085f24ca38a39f4a0cd65423db3cce848e
Tristan F. <leodog896@gmail.com>
2024-01-17 14:27:20 +0000

    feat: base sponsor categories

--- src/routes/sponsor/+page.svelte
+++ src/routes/sponsor/+page.svelte
@@ -1,11 +1,81 @@
+<script lang="ts">
+	// Diamond Sponsors ($5,000+)
+	// Gold Sponsors ($2,500+)
+	// Silver Sponsors ($1,000+)
+	// Bronze Sponsors ($500+)
+	// Honorable Mentions ($100+)
+
+	interface SponsorCategory {
+		name: string;
+		money: number;
+		color: string;
+		textColor?: string;
+	}
+
+	const sponsorCategories: SponsorCategory[] = [{
+		name: "Diamond Sponsors",
+		money: 5000,
+		color: "#3B82F6"
+	}, {
+		name: "Gold Sponsors",
+		money: 2500,
+		color: "#E8C547"
+	}, {
+		name: "Silver Spnsors",
+		money: 1000,
+		color: "#C2DFE3",
+		textColor: "black",
+	}, {
+		name: "Bronze Sponsors",
+		money: 500,
+		color: "#FC7753"
+	}, {
+		name: "Honorable Mentions",
+		money: 100,
+		color: "#717568"
+	}]
+
+</script>
+
 <svelte:head>
 	<title>Deep Sea Tactics - Sponsors</title>
 </svelte:head>
 
-<h1>Sponsor Us</h1>
+<h1>Sponsors</h1>
+
+<main>
+	{#each sponsorCategories as sponsorCategory}
+		<div class="sponsorCategory" style="--color: {sponsorCategory.color}; --textColor: {sponsorCategory.textColor};">
+			<h2>{sponsorCategory.name} - ${sponsorCategory.money}</h2>
+		</div>
+	{/each}
+</main>
 
 <style>
-	h1 {
+	h1, h2 {
 		text-align: center;
+	}
+
+	h1 {
+		font-size: 6rem;
+	}
+
+	h2 {
+		font-size: 3rem;
+	}
+	
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.sponsorCategory {
+		background-color: var(--color);
+		margin: 1rem;
+		width: 800px;
+		color: var(--textColor, white);
+		padding: 1rem;
+		border-radius: 1rem;
 	}
 </style>
